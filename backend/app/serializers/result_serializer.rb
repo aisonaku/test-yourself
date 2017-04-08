9bd857e10fc7d6d96cdc3887ec83e7c4fc0052b7
kuznetsova-oksana <kuznetsova.oksana.w@gmail.com>
2017-04-08 22:45:24 +0300

    Change JSON API structure for results

--- backend/app/serializers/result_serializer.rb
+++ backend/app/serializers/result_serializer.rb
@@ -1,3 +1,8 @@
 class ResultSerializer < ActiveModel::Serializer
-  attributes :value
+  attributes :value, :quiz
+
+  def quiz
+    serializer = ActiveModel::Serializer.serializer_for(object.quiz).new(object.quiz)
+    ActiveModel::Serializer::Adapter::JsonApi.new(serializer).as_json
+  end
 end
