6fa7195f4c201d39d34665093dbc5193ae090b84
kuznetsova-oksana <kuznetsova.oksana.w@gmail.com>
2017-04-08 23:28:49 +0300

    Serialize question text as html

--- backend/app/serializers/question_serializer.rb
+++ backend/app/serializers/question_serializer.rb
@@ -1,6 +1,10 @@
 class QuestionSerializer < ActiveModel::Serializer
   attributes :text, :answers
 
+  def text
+    object.text.html_safe
+  end
+
   def answers
     ActiveModelSerializers::SerializableResource.new(object.answers).serializable_hash
   end
