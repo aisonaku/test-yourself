38a5c52782f17ed1750104a6b60f5db8f4575009
kuznetsova-oksana <kuznetsova.oksana.w@gmail.com>
2017-04-09 18:04:20 +0300

    Fix results JSON

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -2,5 +2,4 @@ class Api::V1::UsersController < ApplicationController
   def index
 		render json: current_user
 	end
-
 end
--- backend/app/serializers/quiz_serializer.rb
+++ backend/app/serializers/quiz_serializer.rb
@@ -1,5 +1,11 @@
 class QuizSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :category
+  attributes :id, :title, :description, :category, :completed
+
+  def completed
+    user = defined?(current_user) ? current_user : scope[:current_user]
+    result = Result.find_by(quiz: object, user: user)
+    result.present?
+  end
 
   def category
     serializer = ActiveModel::Serializer.serializer_for(object.category).new(object.category)
