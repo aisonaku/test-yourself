d3e26dc26630ea636e87d604d14ca284c6c03ae4
kuznetsova-oksana <kuznetsova.oksana.w@gmail.com>
2017-04-11 00:08:34 +0300

    Implement registration validation

--- backend/app/controllers/api/v1/users/registrations_controller.rb
+++ backend/app/controllers/api/v1/users/registrations_controller.rb
@@ -1,19 +1,23 @@
 class Api::V1::Users::RegistrationsController < ApplicationController
   # skip_before_action :authenticate!
-  
+
   def create
-    if params[:confirmation_password] == params[:password]
+    if params[:confirmation_password] == params[:password] 
       username = params[:name]
       password = Digest::SHA1.hexdigest(params[:password])
       if User.find_by(name: username, password: password)
         render status: 422, json: { error: 'Пользователь с таким именем уже существует' }
       else
-        user = User.create(
+        user = User.new(
           name: username, 
           password: password,
           auth_token: SecureRandom.hex)
-        cookies[:auth_token] = user.auth_token
-        render json: { auth_token: user.auth_token }
+        if user.save
+          cookies[:auth_token] = user.auth_token
+          render json: { auth_token: user.auth_token }
+        else
+          render  status: 422, json: { error: 'Пароль и имя пользователя не могут быть пустыми' }
+        end
       end
 
     else
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
 	has_many :user_answers
   has_many :results
+
+  validates :name, :password, presence: true
 end
