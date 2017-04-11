de6218523cc385f359aa55b75c1f333331b672a3
ash <shibakow@gmail.com>
2017-04-11 22:01:02 +0300

    Поправил карточку квеста

--- frontend/app/models/question.js
+++ frontend/app/models/question.js
@@ -4,13 +4,22 @@ import Ember from 'ember';
 export default DS.Model.extend({
    text: DS.attr(),
    answers: DS.attr(),
-   answersArray: Ember.computed('category', function() {
-      let arr = this.get('answers.data').map(item => {
+   answersTop: Ember.computed('category', function() {
+      let arr = this.get('answers.data').map((item, index) => {
          return {
             id: item.id,
             value: item.attributes.text
          };
-      });
+      }).splice(0, 2);
+      return arr;
+   }),
+   answersBottom: Ember.computed('category', function() {
+      let arr = this.get('answers.data').map((item, index) => {
+         return {
+            id: item.id,
+            value: item.attributes.text
+         };
+      }).splice(2);
       return arr;
    })
 });
--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -30,6 +30,9 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
+  ENV['ember-cli-mirage'] = {
+    enabled: false
+  }
   ENV['ember-simple-auth'] = {
     authenticationRoute: 'auth',
     authorizer: 'authorizer:token'
