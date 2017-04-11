3754d310a6e7d1d21e76aa4082a35f43c4e2eb5b
kuznetsova-oksana <kuznetsova.oksana.w@gmail.com>
2017-04-12 00:38:21 +0300

    Handle empty username and password in login

--- backend/app/controllers/api/v1/users/authentications_controller.rb
+++ backend/app/controllers/api/v1/users/authentications_controller.rb
@@ -2,15 +2,18 @@ class Api::V1::Users::AuthenticationsController < ApplicationController
   skip_before_action :authenticate!
 
   def create
-    username = params[:name]
-    password = Digest::SHA1.hexdigest(params[:password])
-
-    if user = User.find_by(name: username, password: password)
-      user.update_attribute(:auth_token, SecureRandom.hex)
-      cookies[:auth_token] = user.auth_token
-      render json: {auth_token: user.auth_token}
+    if params[:name].present? && params[:password].present?
+      username = params[:name]
+      password = Digest::SHA1.hexdigest(params[:password])
+      if user = User.find_by(name: username, password: password)
+        user.update_attribute(:auth_token, SecureRandom.hex)
+        cookies[:auth_token] = user.auth_token
+        render json: {auth_token: user.auth_token}
+      else
+        render status: 422, json: {error: 'Пользователь не найден'}
+      end
     else
-      render status: 422, json: {error: 'Пользователь не найден'}
+      render  status: 422, json: { error: 'Пароль и имя пользователя не могут быть пустыми' }
     end
   end
 end
